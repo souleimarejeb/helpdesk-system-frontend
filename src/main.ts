09ee58076dd83b08d136160d35d77cddef627fb7
souleimarejeb <80159191+souleimarejeb@users.noreply.github.com>
2025-06-24 10:08:57 +0100

    feat : added new compenents

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,22 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+//   setTimeout(() => 
+// { 
+
+
+function removePreloader() {
+  const preloader = document.getElementById('preloader');
+  if (preloader) {
+    preloader.remove();
+  }
+}
+
+setTimeout(() => {
+  removePreloader();
+  bootstrapApplication(AppComponent, appConfig)
+    .catch(err => console.error(err));
+}, 3000);
