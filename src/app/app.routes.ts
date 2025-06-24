a7b2251b1d20104058452702ec7268973eb035f4
souleimarejeb <80159191+souleimarejeb@users.noreply.github.com>
2025-06-24 14:42:45 +0100

    feat : login page design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    {
+        path: '',
+        component: LayoutComponent,
+        // children: [
+        //   { path: '', component: HomeComponent }, // Default child route
+        //   { path: 'about', component: AboutComponent },
+        //   { path: 'contact', component: ContactComponent },
+    }
 ];
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "./footer/footer.component";
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [FooterComponent, NavbarComponent],
+  standalone: true,
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
