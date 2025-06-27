6f2705af3040a8abf41c568cd3d680a40eccfa51
souleimarejeb <80159191+souleimarejeb@users.noreply.github.com>
2025-06-27 09:48:38 +0100

    feat : ticket form creator

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,22 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { LayoutComponent } from './layout/layout.component';
+import { TicketFormComponent } from './features/ticket-form/ticket-form.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
+
     {
         path: '',
         component: LayoutComponent,
-        // children: [
+        children: [
+            { path: 'create-Ticket', component: TicketFormComponent },
+        ]
         //   { path: '', component: HomeComponent }, // Default child route
         //   { path: 'about', component: AboutComponent },
         //   { path: 'contact', component: ContactComponent },
-    }
+    },
+    { path: 'create-Ticket', component: TicketFormComponent }
+
+
 ];
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "./footer/footer.component";
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [FooterComponent, NavbarComponent],
+  imports: [FooterComponent, NavbarComponent, RouterModule],
   standalone: true,
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
