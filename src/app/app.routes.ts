5a926f3063fa5efda72975fbc408739d2939ede4
raoua <selmiraoua7@gmail.com>
2025-06-27 11:17:41 +0100

    list-dashboard+list-ticket+sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { ListTicketComponent } from './tickets/list-ticket/list-ticket.component';
+import { ListDashboardComponent } from './features/dashboard/list-dashboard/list-dashboard.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
@@ -9,4 +10,7 @@ export const routes: Routes = [
     {path:'tickets',
         component:ListTicketComponent
     },
+    {path:'dashboard',
+        component:ListDashboardComponent
+    }
 ];
---
+++ src/app/features/dashboard/list-dashboard/list-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-dashboard',
+  imports: [],
+  templateUrl: './list-dashboard.component.html',
+  styleUrl: './list-dashboard.component.css'
+})
+export class ListDashboardComponent {
+
+}
